8c7995737e286244c208ddeb8a2bedc8d500a632
aylinxndaaa <dimasnugraha1997@gmail.com>
2024-07-16 08:09:48 +0000

    Update welcome message

--- src/app.js
+++ src/app.js
@@ -3,10 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Docker!');
+  res.send('Hello, Updated Docker!');
 });
 
 app.listen(port, () => {
   console.log(`App running at http://localhost:${port}`);
 });
-
